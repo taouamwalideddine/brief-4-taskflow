209573e681c1e7d212d791c159dcebb9fdc1129b
taouamwalideddine <wtaouam@gmail.com>
2024-11-02 00:58:19 +0100

    made the page responsive, also added comments to make the code more readable in the js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let taskIdCounter = 0;
 let currentTask = null;
-
+// function for opening the pop up menu
 function openTaskModal() {
   document.getElementById('taskTitle').value = '';
   document.getElementById('taskDescription').value = '';
@@ -12,12 +12,15 @@ function openTaskModal() {
   document.getElementById('taskModal').classList.remove('hidden');
   currentTask = null;
 }
+// function for adding new tasks using the pop up menu
+
 function addTask() {
   const title = document.getElementById('taskTitle').value;
   const description = document.getElementById('taskDescription').value;
   const dueDate = document.getElementById('taskDueDate').value;
   const priority = document.getElementById('taskPriority').value;
   const status = document.getElementById('taskStatus').value;
+// condition for  checking if the task title is empty
 
   if (!title) {
     document.getElementById('titleWarning').classList.remove('hidden');
@@ -25,6 +28,7 @@ function addTask() {
   } else {
     document.getElementById('titleWarning').classList.add('hidden');
   }
+//for creating nex  task element
 
   const taskElement = currentTask || document.createElement("div");
   taskElement.className = `task p-2.5 m-1.25 rounded-lg border ${getPriorityColor(priority)}`;
@@ -35,17 +39,18 @@ function addTask() {
     <div class="text-xs text-white">Due: ${dueDate}</div>
     <button class="bg-orange-500 text-white px-2 mt-2 rounded" onclick="openEditModal('${taskElement.id}')">Modify</button>
   `;
-
+// for placing tasks in thier chosen lists
   const targetColumn = status === 'todo' ? 'tasksInTodo' :
                        status === 'inProgress' ? 'tasksInProgress' : 'tasksDone';
   document.getElementById(targetColumn).appendChild(taskElement);
 
   closeModal();
 }
+// function for priority color
 function getPriorityColor(priority) {
   return priority === 'P1' ? 'bg-red-300' : priority === 'P2' ? 'bg-green-800' : 'bg-yellow-600';
 }
-
+// function for editing existing modal
 function openEditModal(taskId) {
   currentTask = document.getElementById(taskId);
   document.getElementById('taskTitle').value = currentTask.querySelector('.font-bold').textContent;
@@ -57,12 +62,12 @@ function openEditModal(taskId) {
 
   document.getElementById('taskModal').classList.remove('hidden');
 }
-
+// function for closing pop ups
 function closeModal() {
   document.getElementById('taskModal').classList.add('hidden');
   currentTask = null;
 }
-
+// function for deleting tasks on clik
 function deleteTask() {
   if (currentTask) {
     currentTask.remove();
